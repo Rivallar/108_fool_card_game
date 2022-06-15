6272e3d136e5c2805534f6d2299f401d18d24e08
sipp <fakemail@fake.com>
2022-06-15 14:46:12 +0300

    upload initial project to github

---
+++ fool_108_main.py
@@ -0,0 +1,110 @@
+import pygame
+import pygame.font
+import random
+from game_classes import Player, Card, Deck, Turn, GameFlags, Button, StartScreenProperties
+import game_functions as gf
+import sys
+
+
+#--------------------------------------------Start-game preparations-------------------------
+pygame.init()
+screen = pygame.display.set_mode((1200, 800))
+pygame.display.set_caption('108_fool')
+bg_color = (87, 168, 88)
+back_img = pygame.image.load('Cards_70x105/back.bmp')
+right_arrow = pygame.image.load('Cards_70x105/arrow.png')
+
+screen_rect = screen.get_rect()
+
+
+
+
+#------------------------------------------Start screen (to enter username)---------------------------------------
+
+st_screen = StartScreenProperties()
+
+username_field = pygame.Rect(screen_rect.centerx - 50, 175, 350, 50)
+username_field_border = pygame.Rect(username_field.left - 2, username_field.top - 2, 354, 54)
+
+yellow_rect = pygame.Rect(screen_rect.centerx + 100, screen_rect.centery - 100, 100, 50)
+quit_but = Button(screen, 'Quit', yellow_rect.width - 4, yellow_rect.height - 4, (20, 20, 20), (240, 240, 0))
+quit_but.reposition(yellow_rect.centerx - screen_rect.centerx, yellow_rect.centery - screen_rect.centery)
+
+yellow_rect2 = pygame.Rect(screen_rect.centerx - 100, screen_rect.centery - 100, 100, 50)
+play_but = Button(screen, 'Play', yellow_rect2.width - 4, yellow_rect2.height - 4, (20, 20, 20), (240, 240, 0))
+play_but.reposition(yellow_rect2.centerx - screen_rect.centerx, yellow_rect2.centery - screen_rect.centery)
+
+
+	
+while st_screen.start_screen_flag:
+	
+	gf.start_screen_events(st_screen, quit_but, play_but, username_field)
+	gf.draw_start_screen(screen, screen_rect, st_screen, yellow_rect, yellow_rect2, play_but, quit_but)	
+	
+	if st_screen.name_focus:
+		screen.fill(st_screen.text_color, username_field_border)
+	screen.fill((75, 75, 75), username_field)
+	
+	gf.draw_text_line(st_screen.hint_font, st_screen.username_str, st_screen.text_color, username_field.left + 2, username_field.centery, screen)
+	
+	pygame.display.flip()
+
+#--------------------------------------------Game preparations-----------------------------------------------------
+#Creating players
+players = []
+players.append(Player(st_screen.username_str, bot=False))
+[players.append(Player(name)) for name in Player.bot_names]
+random.shuffle(players)													#random order each game
+
+#Creating cards and card decks
+play_deck = []															#created once, copied to active_deck each round
+for key in Card.points.keys():
+    for suit in Card.suit_names.keys():
+        play_deck.append(Card(key, suit))
+        
+queen_cards = gf.make_queen_cards(screen_rect)							#additional cards for queens cards
+
+active_deck = Deck()
+used_deck = Deck()
+
+#Creating button objects
+cancel_button = Button(screen, 'Cancel', 200, 50, (255, 0, 0))
+one_card_button = Button(screen, 'One!', 100, 50, (255, 0, 0))
+
+play_again_button = Button(screen, 'Play again', 200, 50, (255, 0, 0))
+play_again_button.reposition(-70, 50)
+
+quit_button = Button(screen, 'Quit', 100, 60, (255, 0, 0))
+quit_button.reposition(150, 50)
+
+
+flags = GameFlags()
+
+
+#--------------------------------------------Start-round preparations---------------------------------
+
+gf.reset_decks_and_flags(play_deck, active_deck, used_deck, flags)
+gf.fill_players_hands(players, play_deck, active_deck, used_deck, queen_cards, flags)
+turn = Turn(players[0])
+gf.make_first_turn(turn, players, flags, active_deck, used_deck, queen_cards)			#first turn in each round starts with a random card of first player
+
+
+#-----------------------------------------------------Game cicle------------------------------------
+
+
+while True:
+			
+	if turn.player.bot and not flags.game_over_flag:
+		gf.bot_turn(turn, bg_color, screen_rect, screen, players, back_img, used_deck, active_deck, right_arrow, flags, queen_cards, cancel_button, one_card_button)
+				
+	else:	
+		gf.check_events(turn, used_deck, players, flags, active_deck, queen_cards, cancel_button, one_card_button, quit_button, play_again_button, play_deck)
+
+	if not flags.end_game_flag:
+		gf.draw_everything(bg_color, screen_rect, screen, players, back_img, used_deck, active_deck, right_arrow, flags, queen_cards, cancel_button, one_card_button)
+	
+	elif flags.game_over_flag:
+		gf.draw_end_game_screen(bg_color, screen_rect, screen, players, flags, play_again_button, quit_button)
+	else:
+		gf.new_round(play_deck, active_deck, used_deck, players, queen_cards, flags, turn, screen_rect, screen)
+	
---
+++ game_classes.py
@@ -0,0 +1,277 @@
+import pygame
+import pygame.font
+import random
+  
+class Card:
+	
+	'''Represents card properties and images'''
+	
+	points = {'6':6, '7':7, '8':8, '9':0, '10':10, 'J':2, 'Q':3, 'A':11}	# points to count when a round is over
+	suit_names = {'H': 'HEARTS', 'D': 'DIAMONDS', 'S': 'SPADES', 'C': 'CLUBS'}
+	
+	def __init__(self, value, suit):
+		self.value = value
+		self.suit = suit
+		if self.value:																
+			self.points = Card.points[self.value]
+			self.name = self.value + self.suit
+		else:
+			self.name = Card.suit_names[self.suit]							# special queen cards have no value and points
+		self.image = pygame.image.load(f'Cards_70x105/{self.name}.bmp')
+		self.focus = False													# to focus on a card in a player hand
+		
+	def __repr__(self):
+		return self.name   
+
+class Deck:
+	
+	'''Keeps cards, gives cards to players, manages reshuffling when active deck is empty'''
+	
+	def __init__(self, cards=[]):
+		self.cards = cards
+		
+	def give_card(self, player, quantity, used_deck, queen_cards, flags, reverse=False):
+		
+		'''Gives cards from active deck to players hands. Checks if there are enough cards in the deck.'''
+		
+		cards_left = len(self.cards)
+		if cards_left == 0 and len(used_deck.cards) == 1:					# all cards in players hands
+			return None
+		if cards_left >= quantity:
+			for _ in range(quantity):
+				if reverse:														
+					player.hand.append(self.cards.pop())					# return last card (queen) to active player when he cancels
+				else:
+					player.hand.append(self.cards.pop(0))
+		else:																# not enough cards in active deck
+			self.reshuffle(player, quantity, used_deck, queen_cards, cards_left, flags)
+
+	def reshuffle(self, player, quantity, used_deck, queen_cards, cards_left, flags):
+		
+		'''Gives to a player last cards in active deck, removes queen cards from a used deck,
+		reshuffles used deck to active deck, gives to a player remaining cards and does penalty for reshuffling'''
+		
+		player.hand += self.cards
+		quantity -= cards_left
+		
+		print(f'Player {player} takes additional {5 * flags.reshuffle_count} points for reshuffle')	#log
+		player.round_points += 5 * flags.reshuffle_count					# penalty points for reshuffling. Should be taken from settings
+		flags.reshuffle_count += 1
+		
+		cards_to_shuffle = list(set(used_deck.cards[:-1]))					# remove queen cards
+		for card in queen_cards:
+			if card in cards_to_shuffle:
+				cards_to_shuffle.remove(card)
+		
+		self.cards = cards_to_shuffle
+		random.shuffle(self.cards)
+		
+		used_deck.cards = used_deck.cards[-1:]
+		self.give_card(player, quantity, used_deck, queen_cards, flags)
+
+	
+        
+        
+class Player:
+	
+	'''Represents bots and real players. Keeps players cards, manages bots and players turns, counts points
+	in the end of a round'''
+	
+	bot_names = ['BOT1', 'BOT2',]
+	
+	def __init__(self, name, bot=True):
+		self.name = name
+		self.bot = bot
+		self.hand = []													# players cards
+		self.active_flag = False										# players turn flag
+		self.one_card_flag = False
+		
+		self.total_points = 0
+		self.round_points = 0
+		
+		
+		#Drawing name
+		self.text_color = (240, 240, 0)
+		self.back_color = (0, 100, 175)
+		self.font = pygame.font.SysFont(None, 36)
+		self.name_img = self.font.render(self.name, True, self.text_color, self.back_color)
+		
+	def choose_useful_cards(self, used_deck_last_card):
+		
+		'''To determine best card for bot turn'''
+		
+		useful_cards = set()
+		for card in self.hand:
+			if card.value == 'Q':
+				useful_cards.add(card)
+			elif card.value == used_deck_last_card.value or card.suit == used_deck_last_card.suit:
+				useful_cards.add(card)
+		best_card = self.choose_best_card(useful_cards)
+		return useful_cards, best_card
+	
+	def choose_best_card(self, useful_cards):
+		
+		'''To determine best card (max points) for bot turn'''
+		
+		if useful_cards:
+			best_card = max(useful_cards, key=lambda card: card.points)
+			return best_card
+		
+	def count_round_points(self):
+		
+		'''Counting end-round points'''
+		
+		if len(self.hand) == 1 and self.hand[0].value == 'Q':			# penalty if only queen left
+			if self.hand[0].suit == 'S':
+				self.round_points += 40
+			else:
+				self.round_points += 20
+		else:
+			for card in self.hand:
+				self.round_points += card.points
+		return self.round_points
+		
+		
+		
+	def __repr__(self):
+		return self.name
+		
+	def use_card(self, card_id, used_deck):
+		
+		'''Make turn if given card can be used'''
+		
+		if not used_deck.cards:
+			used_deck.cards.append(self.hand.pop(card_id))
+		elif self.hand[card_id].suit == used_deck.cards[-1].suit or self.hand[card_id].value == used_deck.cards[-1].value or self.hand[card_id].value == 'Q':
+			used_deck.cards.append(self.hand.pop(card_id))
+
+	def take_card(self, active_deck, used_deck, queen_cards, flags):
+		
+		'''Take a card from a deck if there are no playable cards in a hand'''
+		
+		active_deck.give_card(self, 1, used_deck, queen_cards, flags)
+		        
+                
+class Turn:
+	
+	'''Returns an active player to start-turn state: sorted cards, first card in focus'''
+	
+	def __init__(self, player):
+		self.player = player
+		self.player.hand.sort(key=Turn.sort_hand)
+		self.player.active_flag = True
+		self.reset_focus()
+	
+	
+	def reset_focus(self):
+		
+		'''Only first card in focus'''
+		
+		self.player.hand[0].focus = True
+		for card in self.player.hand[1:]:
+			card.focus = False
+		self.card_taken_flag = False
+		self.focus_index = 0
+		
+	@staticmethod	
+	def sort_hand(card):
+		return card.suit, card.points
+
+class GameFlags:
+	
+	'''Keeps important settings and information about game-state'''
+	
+	def __init__(self):
+	
+		self.loose_points = 15											# points to loose the game should be taken from settings
+		self.first_player_ind = 0
+		
+		self.text_color = (240, 240, 0)
+		self.font = pygame.font.SysFont(None, 36)
+		self.reset_flags()
+		
+		
+	def reset_flags(self):
+		
+		self.game_over_flag = False
+		self.end_game_flag = False
+		self.reverse_flag = False
+		self.queen_choose_flag = False
+		self.first_turn_flag = True
+		self.loosers = []
+		
+		self.reshuffle_count = 1
+		
+		self.prep_loose_score()
+		
+	def prep_loose_score(self):
+		
+		'''To show loose-points on the screen during the game'''
+		
+		self.loose_string = f'Loose points: {self.loose_points}'
+		self.loose_score_img = self.font.render(self.loose_string, True, self.text_color)
+		self.loose_score_rect = self.loose_score_img.get_rect()
+		
+
+class Button:
+	
+	''' Manages in-game buttons'''
+	
+	def __init__(self, screen, msg, width, height, button_color, text_color=(255, 255, 255)):
+		
+		self.screen = screen
+		
+		self.screen_rect = self.screen.get_rect()
+		
+		#dimensions&properties of the button
+		self.width, self.height = width, height
+		self.button_color = button_color
+		self.text_color = text_color
+		self.font = pygame.font.SysFont(None, 48)
+		
+		self.rect = pygame.Rect(0, 0, self.width, self.height)
+		self.rect.center = self.screen_rect.center						
+		
+		self.prep_msg(msg)
+		
+	def prep_msg(self, msg):
+		
+		'''Renders text string to image.'''
+		
+		self.msg_image = self.font.render(msg, True, self.text_color, self.button_color)
+		self.msg_image_rect = self.msg_image.get_rect()
+		self.msg_image_rect.center = self.rect.center
+		
+	def reposition(self, x_shift, y_shift):
+		
+		'''To shift a button to correct place'''
+		
+		self.msg_image_rect.centerx = self.screen_rect.centerx + x_shift
+		self.msg_image_rect.centery = self.screen_rect.centery + y_shift
+		self.rect.center = self.msg_image_rect.center
+		
+	def draw_button(self):
+		self.screen.fill(self.button_color, self.rect)
+		self.screen.blit(self.msg_image, self.msg_image_rect)
+
+class StartScreenProperties:
+	
+	'''Represents initial screen to fill your name and read hints'''
+	
+	def __init__(self):
+		self.name_focus = False
+		self.start_screen_flag = True
+		
+		self.start_screen_color = (0, 0, 0)
+		self.text_color = (240, 240, 0)
+		
+		self.big_header = '108 FOOL GAME'
+		self.header_font = pygame.font.SysFont(None, 66)
+		self.hint_font = pygame.font.SysFont(None, 42)
+		
+		self.username_str = ''
+		self.hints = hints = [
+			'> Press Q to quit',
+			'> Use arrows to navigate through your cards',
+			'> Press ENTER button to use the selected card',
+			'> SPACEBAR to draw a card or pass your turn']
